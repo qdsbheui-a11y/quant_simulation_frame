4f9424ec03108e697803f0f675f5cf8c3a59f690
qdsbheui-a11y <qdsbheui@gmail.com>
2026-05-10 21:39:47 +0800

    Export HTML backtest report helpers

--- python-bridge/backtest/__init__.py
+++ python-bridge/backtest/__init__.py
@@ -2,6 +2,7 @@
 
 from .data import CsvDailyBarDataSource, DailyBarDataSource, InMemoryDailyBarDataSource
 from .models import BacktestConfig, BacktestResult, BacktestTrade, DailyBar, EquitySnapshot, PositionState, TargetWeight
+from .report import calculate_mt_style_statistics, write_html_report
 from .runner import BacktestRunner
 from .strategy import BacktestContext, BacktestStrategy, EqualWeightSmallCapStrategy, EqualWeightUniverseStrategy
 
@@ -21,4 +22,6 @@ __all__ = [
     "InMemoryDailyBarDataSource",
     "PositionState",
     "TargetWeight",
+    "calculate_mt_style_statistics",
+    "write_html_report",
 ]
